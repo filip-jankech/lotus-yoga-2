f417b0e7ebecf58f332947e0e198540a1c0eb1c4
Filip Jankech <filip.jankech@gmail.com>
2020-12-14 15:28:31 +0100

    seventh

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,14 +22,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-htaccess',
-      options: {
-        https: true,
-        www: false,
-        host: 'lotusyoga.cz',
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
